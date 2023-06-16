0d818d334bf96b296f69011b4ffe0304e054559a
Approachablemember <marmariz01ua@icloud.com>
2023-06-16 21:08:41 +0200

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
   overflow-y: auto;
   scrollbar-width: none;
   color: white;
-  background: url(/img/bg.jpeg);
+  background: url("./img/bg.jpeg");
   &::-webkit-scrollbar{
     display: none;
   }
